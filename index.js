437b9e81d0cae091d76bbef05af9a6a6b1951e6c
ricardo1181 <ricardo39985@gmail.com>
2022-09-20 15:11:55 -0400

    Add new variables. Cache Elements. Update createcards

--- index.js
+++ index.js
@@ -15,10 +15,41 @@ const ranks = [
 ];
 const suits = ["c", "d", "h", "s"];
 const cards = [];
-let playerHand = [];
-let computerHand = [];
+const playerHand = [];
+const computerHand = [];
+
+const players = {
+  user: {
+    lastCardVal: 0,
+    handsWon: 0
+  },
+  computer: {
+    lastCardVal: 0,
+    handsWon: 0
+  },
+};
 createCards();
 dealCards();
+//CACHED ELEMENTS
+const tableSpaces = document.querySelector(".table").childNodes;
+const playerSpace = tableSpaces[1];
+const computerSpace = tableSpaces[3];
+const playButton = document.querySelector(".play-button div");
+playButton.addEventListener("click", (e) => {
+  console.log(e.target);
+  computerSpace.setAttribute(
+    "class",
+    `card ${
+      pickRandomCard("user")[0].suit + pickRandomCard("computer")[0].face
+    }`
+  );
+  playerSpace.setAttribute(
+    "class",
+    `card ${pickRandomCard("user")[0].suit + pickRandomCard("user")[0].face}`
+  );
+
+  console.log(pickRandomCard("user")[0].suit + pickRandomCard("user")[0].face);
+});
 
 // HELPERS
 function createCards() {
@@ -30,18 +61,28 @@ function createCards() {
       switch (rank) {
         case "Jack":
           newCard.value = 11;
+          newCard.face = "J";
+
           break;
         case "Queen":
           newCard.value = 12;
+          newCard.face = "Q";
+
           break;
         case "King":
           newCard.value = 13;
+          newCard.face = "K";
+
           break;
         case "Ace":
           newCard.value = 14;
+          newCard.face = "A";
+
           break;
         default:
           newCard.value = parseInt(rank);
+          newCard.face = rank;
+
           break;
       }
       cards.push(newCard);
@@ -51,8 +92,17 @@ function createCards() {
 function dealCards() {
   let randomIndex = () => Math.floor(Math.random() * cards.length);
   while (cards.length > 0) {
-    playerHand.push(cards.splice(randomIndex, 1));
-    computerHand.push(cards.splice(randomIndex, 1));
+    computerHand.push(cards.splice(randomIndex(), 1));
+    playerHand.push(cards.splice(randomIndex(), 1));
   }
 }
-console.log(computerHand.length);
+
+function pickRandomCard(player) {
+  if (player === "user") {
+    return playerHand[Math.floor(Math.random() * playerHand.length)];
+  }
+  if (player === "computer") {
+    return computerHand[Math.floor(Math.random() * computerHand.length)];
+  }
+}
+console.log(playerHand);
