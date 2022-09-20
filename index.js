855eeded34538f1784355c53c5ec050ad4dc5fef
ricardo1181 <ricardo39985@gmail.com>
2022-09-20 19:19:37 -0400

    update Event Listener

--- index.js
+++ index.js
@@ -30,26 +30,59 @@ const players = {
     lastCard: {},
   },
 };
-
+const table = [];
 init();
 //CACHED ELEMENTS
 const tableSpaces = document.querySelector(".table").childNodes;
 const playerSpace = tableSpaces[1];
 const computerSpace = tableSpaces[3];
 const playButton = document.querySelector(".play-button div");
-const playerScoreEl = document.querySelectorAll('h4')[0]
-const computerScoreEl = document.querySelectorAll('h4')[1]
+const playerScoreEl = document.querySelectorAll("h4")[0];
+const computerScoreEl = document.querySelectorAll("h4")[1];
 
-console.log(computerScoreEl)
+console.log(computerScoreEl);
 playButton.addEventListener("click", (e) => {
   pickRandomCard();
-  const playerCard = players.player.lastCard
-  const computerCard = players.computer.lastCard
+  const playerCard = players.user.lastCard;
+  const computerCard = players.computer.lastCard;
 
-  console.log(players.computer.lastCard)
-  computerSpace.setAttribute("class", `card ${computerCard.suit+computerCard.face}`);
-  playerSpace.setAttribute("class", `card ${playerCard.suit+playerCard.face}`);
+  resolveWinner(playerCard, computerCard);
+  console.log(players.computer.lastCard);
+  render(computerCard, playerCard);
 });
+
+function render(computerCard, playerCard) {
+  computerSpace.setAttribute(
+    "class",
+    `card ${computerCard.suit + computerCard.face}`
+  );
+  playerSpace.setAttribute(
+    "class",
+    `card ${playerCard.suit + playerCard.face}`
+  );
+  computerScoreEl.innerText = players.computer.handsWon;
+  playerScoreEl.innerText = players.user.handsWon;
+}
+
+function resolveWinner(playerCard, computerCard) {
+  // while (playerCard.value === computerCard.value) {
+  //   pickRandomCard();
+  // }
+  playerCard.value > computerCard.value
+    ? (players.user.handsWon += 1)
+    : (players.computer.handsWon += 1);
+  if (playerCard.value > computerCard.value) {
+    table.forEach((index) => {
+      players.user.hand.push(table.splice(index, 1)[0]);
+    });
+    document.querySelectorAll(".message")[0].innerText = "Player wins";
+  } else {
+    table.forEach((index) => {
+      players.computer.hand.push(table.splice(index, 1)[0]);
+    });
+    document.querySelectorAll(".message")[0].innerText = "Computer wins";
+  }
+}
 
 // HELPERS
 function createCards() {
@@ -98,12 +131,17 @@ function dealCards() {
 }
 
 function pickRandomCard() {
-  players.user.lastCard =
-    players.user.hand[Math.floor(Math.random() * players.user.hand.length)];
-  players.computer.lastCard =
-    players.computer.hand[
-      Math.floor(Math.random() * players.computer.hand.length)
-    ];
+  let pCard = players.user.hand.splice(
+    Math.floor(Math.random() * players.user.hand.length),
+    1
+  )[0];
+  let cCard = players.computer.hand.splice(
+    Math.floor(Math.random() * players.computer.hand.length),
+    1
+  )[0];
+  table.push(pCard, cCard);
+  players.user.lastCard = pCard;
+  players.computer.lastCard = cCard;
 }
 
 function isGameWon() {
